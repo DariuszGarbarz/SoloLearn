f5ba80dfa444b2e5e2c8573afe03c2d845bc100b
DariuszGarbarz <plgarbarda@gmail.com>
2021-12-13 13:01:29 +0100

    2 out of 9 excercises rewrited with Unit Tests

---
+++ BasicConcepts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SoloLearn
+{
+    /* Problem Explanation
+     *Area of a Circle
+
+
+We are going to write a program that will calculate the area of ​​a circle.
+The area enclosed by a circle of radius r is πr², where π (pi) is the constant ratio of the circumference of any circle to its diameter, and r is the radius.
+The given program declares a constant pi variable with a value of 3.14.
+Complete the program to take the radius as input, then calculate and output the area of the circle.
+
+Sample Input
+5
+
+Sample Output
+78.5
+Remember to convert the inputted value to type double.
+     *
+     * */
+
+
+    public static class BasicConcepts
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius;
+
+            //your code goes here
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            var output = AreaOfCircle(radius, pi);
+            Console.WriteLine(output);
+
+        }
+
+        public static double AreaOfCircle(double radius, double pi)
+        {
+            double output = pi * radius * radius;
+            return output;
+        }
+    }
+}
---
+++ ConditionalsAndLoops.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SoloLearn
+{
+    /* Problem Explanation
+     * Multiple of 3
+You are an elementary school teacher and explaining multiplication to students.
+You are going to use multiplication by 3 as your example.
+The program you are given takes N number as input. Write a program to output all numbers from 1 to N, replacing all numbers that are multiples of 3 by "*".
+
+Sample Input
+7
+
+Sample Output
+12*45*7
+The N number is a multiple of 3 if N%3==0.
+     * 
+     */
+    public static class ConditionalsAndLoops
+    {
+        static void Main1(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //your code goes here
+            int multiple = 3;
+
+            Console.WriteLine(Multiples(number, multiple));
+        }
+        public static string Multiples(int number, int multiple)
+        {
+            string result = null;
+            for (int x = 1; x <= number; x++)
+            {
+                if (x % multiple == 0)
+                {
+                    result += "*, ";
+                }
+                else
+                {
+                    result += x + ", ";
+                }
+            }
+            return result;
+        }
+    }
+}
+
