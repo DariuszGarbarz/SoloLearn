83be08e873f559d2e46b4d31e2da03406fae1cbc
DariuszGarbarz <plgarbarda@gmail.com>
2021-12-13 14:24:55 +0100

    Solution Fixed, 3 out of 9 excercises rewrited with unit tests.

---
+++ SoloLearn/Methods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoloLearn
+{
+    /* Problem Explanation
+ * Level Points
+
+
+Passing the first level of a video game awards the player 1 point. For each subsequent level passed, the points awarded increment by 1 (2 for the 2nd level, 3 for the 3rd, and so on).
+The program you are given takes the number of passed levels as input. Complete the given function to take that number as an argument, and recursively calculate and return the total number of points given for all passed levels.
+
+Sample Input
+3
+
+Sample Output
+6
+
+Explanation
+Level 1: 1 point
+Level 2: 2 points
+Level 3: 3 points
+Total: 1+2+3 = 6 points.
+Don't forget about the base case.
+ * 
+ * 
+ */
+    public class Methods
+    {
+        static void Main2(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Points(levels));
+        }
+
+
+        public static int Points(int levels)
+        {
+            //your code goes here
+            int sum = 0;
+
+            for (int i = levels; i > 0; i--)
+            {
+                sum += i;
+            }
+            return sum;
+
+        }
+
+    }
+
+}
---
+++ SoloLearnTests/MethodsTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SoloLearn;
+
+namespace SoloLearnTests
+{
+    [TestClass]
+    public class MethodsTest
+    {
+        [TestMethod]
+        public void PointsTest()
+        {
+            int case1 = 4;
+            int case2 = 2;
+            int case3 = 10;
+            int expected1 = 10;
+            int expected2 = 3;
+            int expected3 = 55;
+
+            int actual1 = Methods.Points(case1);
+            int actual2 = Methods.Points(case2);
+            int actual3 = Methods.Points(case3);
+
+            Assert.AreEqual(expected1, actual1);
+            Assert.AreEqual(expected2, actual2);
+            Assert.AreEqual(expected3, actual3);
+        }
+    }
+}
